62c833d6f88702b34f5dd3b1c685607d395637f4
MANAN-GAJJAR07 <mnngajjarashokbhai@gmail.com>
2026-09-16 18:15:07 +0530

    Add files via upload

---
+++ SE/C/Assignment2/Session10/t1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char songTitle[] = "Tum Hi Ho";
+
+    printf("Song: %s\n", songTitle);
+    printf("Length: %lu", strlen(songTitle));
+
+    return 0;
+}
---
+++ SE/C/Assignment2/Session10/t2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char username1[50];
+    char username2[50];
+
+    printf("Enter first username: ");
+    scanf("%49s", username1);
+
+    printf("Enter second username: ");
+    scanf("%49s", username2);
+
+    if(strcmp(username1, username2) == 0)
+    {
+        printf("Both usernames are same.");
+    }
+    else
+    {
+        printf("Usernames are different.");
+    }
+
+    return 0;
+}
