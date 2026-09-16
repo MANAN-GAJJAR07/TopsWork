52c3a1ad7920ed7db1c3e46feab69104e1e18490
MANAN-GAJJAR07 <mnngajjarashokbhai@gmail.com>
2026-09-16 18:11:04 +0530

    Add files via upload

---
+++ SE/C/Assignment2/Session7/pyramid.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, j, space;
+
+    for(i = 1; i <= 6; i++)
+    {
+        // Print spaces
+        for(space = 1; space <= 6 - i; space++)
+        {
+            printf(" ");
+        }
+
+        // Print stars
+        for(j = 1; j <= 2 * i - 1; j++)
+        {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
